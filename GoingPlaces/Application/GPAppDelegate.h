f380d3fdf4206540f0777663df6781f44d9ce8f8
Mihaela Mihaljevic Jakic <mihaelamj@me.com>
2014-05-17 18:07:11 +0200

    initial application file and group structure

---
+++ GoingPlaces/Application/GPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GPAppDelegate.h
+//  GoingPlaces
+//
+//  Created by Mihaela Mihaljević Jakić on 17/05/14.
+//  Copyright (c) 2014 Token d.o.o. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GoingPlaces/GoingPlacesTests/GoingPlacesTests.m
@@ -0,0 +1,34 @@
+//
+//  GoingPlacesTests.m
+//  GoingPlacesTests
+//
+//  Created by Mihaela Mihaljević Jakić on 17/05/14.
+//  Copyright (c) 2014 Token d.o.o. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GoingPlacesTests : XCTestCase
+
+@end
+
+@implementation GoingPlacesTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
