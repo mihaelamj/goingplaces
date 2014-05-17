c0a88980c584675ad722037863089c6b2fcbc1e2
Mihaela Mihaljevic Jakic <mihaelamj@me.com>
2014-05-17 19:17:48 +0200

    added GooglePlaces pod

---
+++ GoingPlaces/GoingPlaces/Internals/Config/Keys.h
@@ -0,0 +1,14 @@
+//
+//  Keys.h
+//  GoingPlaces
+//
+//  Created by Mihaela Mihaljević Jakić on 17/05/14.
+//  Copyright (c) 2014 Token d.o.o. All rights reserved.
+//
+
+#ifndef GoingPlaces_Keys_h
+#define GoingPlaces_Keys_h
+
+#define GOOGLE_API_KEY @"AIzaSyCU4oAydzave9NI09HZoPWp8uKqcuyL2BQ"
+
+#endif
---
+++ GoingPlaces/Pods/Pods-FTGooglePlacesAPI-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FTGooglePlacesAPI : NSObject
+@end
+@implementation PodsDummy_Pods_FTGooglePlacesAPI
+@end
